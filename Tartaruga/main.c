0a1fa74d7bc3da4f124d57c7620263a444aad9ab
Rodrigo Skurczynski <57725608+Lukical@users.noreply.github.com>
2021-11-15 18:28:05 -0300

    Troca para omp

--- Tartaruga/main.c
+++ Tartaruga/main.c
@@ -1,34 +1,25 @@
 /* Rodrigo Skurczynski - 41905113 - 5G */
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <time.h>
+#include <omp.h>
 
 int thread_count = 4;
-long double sum = 0;
-void *Taylor(void* arg);
-int main() //main
-{
-    long int l = 1;
-    long thread;
-    pthread_t* thread_handles; //cria threads
-    thread_handles = malloc(thread_count*sizeof(pthread_t));//alocação
-    time_t start = time(NULL); 
-    while(1)
-    {
-        for(thread = 0;thread < thread_count;thread++)
-        {
-            pthread_create(&thread_handles[thread],NULL,Taylor,&l);
-            pthread_join(thread_handles[thread],NULL);
-            l++;
-        }
-        printf("\n %.20Lf %ld",sum,time(NULL) - start);
-    }
-    free(thread_handles);
+void Taylor(int num,double* sum);
+int main()
+{	
+	long int l = 1;
+	long double sum = 0;
+	time_t start = time(NULL); 
+	while(1)
+	{
+		#pragma omp parallel num_threads(thread_count)
+		Taylor(l++,&sum);
+		printf("\n %.20Lf %ld",sum,time(NULL) - start);
+	}
 }
-void* Taylor(void* arg)
+void Taylor(int num,long double* sum)
 {
-    int* num = (int*)arg;
-    sum += (1.0 / *num);
-    return NULL;
+    #pragma omp critical
+    *sum += (1.0 / num);
 }
